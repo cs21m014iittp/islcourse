7a588c0d88279fead820ea6972d8eabdc470bebb
cs21m014 <90147052+cs21m014iittp@users.noreply.github.com>
2022-11-21 12:49:35 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -253,6 +253,6 @@ def train_combined_encdec_predictor(mynn,X,y, epochs=11):
       ypred, Xencdec = mynn(X)
       lval = mynn.loss_fn(X,y,ypred,Xencdec)
       lval.backward()
-      optimzer.step()
+      optimizer.step()
     
   return mynn
